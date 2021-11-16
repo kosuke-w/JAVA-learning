3b920a0feb1cc1b17a6eeb38a3accc6d115bdf74
kosuke-w <s1lv3rt1psh4rk@gmail.com>
2021-11-16 23:56:01 +0900

    [Finish]chapter4

---
+++ Chapter04.java
@@ -0,0 +1,16 @@
+package Chapter;
+
+public class Chapter04 {
+	public static void main (String[] args) {
+		String str1 = "kosuke-w";
+		System.out.println(str1);
+
+		str1 = "hoge";
+		System.out.println(str1);
+
+		int LackyNumber = 7;
+		System.out.println(LackyNumber);
+
+	}
+
+}
