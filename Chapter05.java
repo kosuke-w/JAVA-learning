41a278213e38cc65aa117559842fb8d67d1477bc
kosuke-w <s1lv3rt1psh4rk@gmail.com>
2021-11-17 00:34:09 +0900

    [Finish]chapter5

---
+++ Chapter05.java
@@ -0,0 +1,23 @@
+package Chapter;
+
+public class Chapter05 {
+	public static void main(String args[]) {
+		long l = 12345678912345L;
+		double d = 0.123;
+		boolean b = true;
+		System.out.println(l);
+		System.out.println(d);
+		System.out.println(b);
+
+		Float f = 0.123f;
+		Integer i = 123;
+		Character c = 'あ';
+		System.out.println(f);
+		System.out.println(i);
+		System.out.println(c);
+
+		int[] array = { 1, 3, 5, 7, 9 };
+		System.out.println(array[3]);
+	}
+
+}
