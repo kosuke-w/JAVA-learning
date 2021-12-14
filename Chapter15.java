e448171a01ec4fc7c2b0bd26b884e07004700bb2
kosuke-w <s1lv3rt1psh4rk@gmail.com>
2021-12-14 21:22:16 +0900

    [Start]chapter15

---
+++ Chapter15.java
@@ -0,0 +1,17 @@
+package Chapter;
+
+public class Chapter15 {
+	public static void main(String[] args) {
+        int a = 976;
+        int b = 427;
+        System.out.println(a + " と " + b + " の最大公約数は、" + gcd(a, b) + "です。");
+    }
+
+    static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+}
